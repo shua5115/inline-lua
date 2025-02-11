3ced1ba76d6a5ef0363db90f4b54b325d5182601
Yehoshua Halle <yehoshua.halle@outlook.com>
2025-02-11 09:14:52 -0500

    wrap_stateless more concise

--- src/literlib.c
+++ src/literlib.c
@@ -868,15 +868,12 @@ static int iter_wrap_stateless(inlua_State *L) {
   inlua_pushvalue(L, inlua_upvalueindex(2));
   inlua_pushvalue(L, inlua_upvalueindex(3));
   inlua_call(L, 2, INLUA_MULTRET); // sets top, first points to first return value
-  if (!inlua_isnone(L, first)) inlua_pushvalue(L, first);
-  else inlua_pushnil(L);
+  (void) inluaL_opt(L, inlua_pushvalue, first, inlua_pushnil(L));
   inlua_replace(L, inlua_upvalueindex(3)); // key = first
   if (inlua_isnoneornil(L, first)) return 0;
   return 1+inlua_gettop(L)-first;
   // 4,  5,  6 -> 6-4 = 2, need 3 returns so add 1
   // ^first  ^top
-  // 1,  -> 1-1=0, need 1 return, so add 1
-  // ^first,top
 }
 
 static int inlua_itercreate(inlua_State *L) {
